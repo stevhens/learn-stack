74b2f76d13272b14fee2e2fa9db25b698eb86045
Stevhen <stevhen.work@gmail.com>
2021-03-10 01:26:02 +0700

    first commit

---
+++ my-widget/learn-stack-widget.swift
@@ -0,0 +1,28 @@
+//
+//  learn-stack-widget.swift
+//  learn-stack
+//
+//  Created by Stevhen on 09/03/21.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct BellWidgetSmall: View {
+    var body: some View {
+        
+        ZStack {
+                
+            //let's code here
+            
+        }
+        
+    }
+}
+
+struct BellWidgetSmall_Previews: PreviewProvider {
+    static var previews: some View {
+        BellWidgetSmall()
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
